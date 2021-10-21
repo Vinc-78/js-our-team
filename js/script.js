a106c7417727f45b51d60ae1a231425be0f911eb
Vinc-78 <vincenzodicostanzo.um@gmail.com>
2021-10-21 15:45:49 +0200

    stampa le card dall'array

--- js/script.js
+++ js/script.js
@@ -42,3 +42,54 @@ console.log("stampa del primo elemento il nome con il dot")
 console.log(arrayTeam[0].name);
 console.log("stampa del primo elemento il ruolo con il propriety")
 console.log(arrayTeam[0]["role"]);
+
+
+
+
+//definisco le variabili che puntano agli elementi del DOM
+
+const teamContainer = document.querySelector(".team-container");
+
+// realizzo la funzione che per ogni elemento dell'array crea la team card e l'aggiunge al DOM
+// funzione evocata da subito
+
+
+
+stampaTutteCard();
+
+function stampaTutteCard() {
+
+    console.log("funziona");
+
+    for (let i=0; i<arrayTeam.length; i++) {
+
+        const elementArray = arrayTeam[i];
+
+        const teamCard = document.createElement("div");
+        teamCard.classList.add("team-card");
+        teamContainer.append(teamCard);
+
+        const imgCard = document.createElement("div");
+        imgCard.classList.add("card-image");
+        imgCard.innerHTML=`<img src="${elementArray.image}" alt="${elementArray.name}" /> `
+        teamCard.append(imgCard);
+
+        const textCard = document.createElement("div");
+        textCard.classList.add("card-text");
+        teamContainer.append(teamCard);
+
+        const h3 =document.createElement("h3");
+        h3.innerHTML=`${elementArray.name}`
+        h3.append(textCard)
+
+        const p =document.createElement("p");
+        p.innerHTML=`${elementArray.role}`
+        p.append(textCard);
+        
+    }
+
+}
+
+
+
+   
